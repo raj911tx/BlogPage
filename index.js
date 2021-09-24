ef028cbafa31fd893f2c2b31d9776c6cc1b32ee4
raj911tx <advancedroboticssystem@gmail.com>
2021-09-24 14:51:50 +0530

    adding mongodb database functionality

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 const express=require('express')
 const path = require('path')
 const ejs=require('ejs')
+const mongoose=require('mongoose')
+const bodyParser = require('body-parser')
+const BlogPost=require('./models/BlogPost')
 
+mongoose.connect('mongodb://localhost/my_database',{useNewUrlParser:true})
 const app=new express()
 app.set('view engine','ejs')
 
 app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+
 app.listen(4000,()=>{
     console.log('App listening on port 4000')
 })
 
-app.get('/',(req,res)=>{
+app.get('/',async (req,res)=>{
     // res.sendFile(path.resolve(__dirname,'pages/index.html'))
-    res.render('index')
+    const blogposts=await BlogPost.find({})
+    res.render('index',{
+        blogposts:blogposts
+    });
+    // console.log(blogposts)
 })
 
 app.get('/contact',(req,res)=>{
-    // res.sendFile(path.resolve(__dirname,'pages/contact.html'))
     res.render('contact')
 })
 
 app.get('/about',(req,res)=>{
-    // res.sendFile(path.resolve(__dirname,'pages/about.html'))
     res.render('about')
 })
 
 app.get('/post',(req,res)=>{
-    // res.sendFile(path.resolve(__dirname,'pages/post.html'))
     res.render('post')
+})
+
+app.get('/post/new',(req,res)=>{
+    res.render('create')
+})
+
+app.post('/posts/store',async (req,res)=>{
+    // console.log(req.body)
+    await BlogPost.create(req.body)
+    res.redirect('/')
 })
---
+++ test.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const BlogPost=require('./models/BlogPost')
+mongoose.connect('mongodb://localhost/my_database',{useNewUrlParser:true});
+
+// BlogPost.create({
+//     title:'Electrical energy efficiency on United States farms',
+//     body:'Hitherto almost entirely unregulated (except for zoning ordinances and the effects of property taxes), environmental concerns are now beginning to impose restrictions on farms'           
+// },(error,blogpost)=>{
+//     console.log(error,blogpost)
+// })
+id="614d7e6a0368c73b4b46a26f"
+
+BlogPost.findById(id,(error,blogpost)=>{
+    console.log(error,blogpost)
+})
+
+// BlogPost.findByIdAndUpdate(id,{title:'Quantum energy efficiency on United States farms'},(error,blogpost)=>{
+//     console.log(error,blogpost)
+// })
